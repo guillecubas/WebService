e898d1ed8bd6f90ab8de019d98a074d2e9ca6ade
guillecubas <114291786+guillecubas@users.noreply.github.com>
2024-03-04 10:32:36 +0100

    Delete Week2Homework directory

--- Week2Homework/src/main/java/homework2/PizzaPopularityService.java
+++
@@ -1,8 +0,0 @@
-package homework2;
-
-@jakarta.jws.WebService
-public interface PizzaPopularityService {
-	@jakarta.jws.WebMethod
-	int getPizzaPopularity(String pizzaType);
-
-}
--- Week2Homework/src/main/java/homework2/PizzaPriceService.java
+++
@@ -1,8 +0,0 @@
-package homework2;
-
-
-@jakarta.jws.WebService
-public interface PizzaPriceService {
-	@jakarta.jws.WebMethod
-    double getPizzaPrice(String pizzaType, int size);
-}
